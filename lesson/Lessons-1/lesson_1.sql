d4830580c8fab25ebcd7a4b21e663da868ebe874
pr0h03iy <kireev.ruslan019@gmail.com>
2025-11-19 16:19:05 +0300

    initial commit

---
+++ lesson/Lessons-1/lesson_1.sql
@@ -0,0 +1,13 @@
+SHOW CREATE TABLE persons;
+
+CREATE TABLE Kireev_Table (
+  id int DEFAULT NULL,
+  name varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
+ALTER TABLE Kireev_Table ADD COLUMN user_name TEXT;
+ALTER TABLE Kireev_Table ADD COLUMN inst_type varchar(100);
+ALTER TABLE Kireev_Table ADD COLUMN inst_name varchar(100);
+INSERT INTO Kireev_Table (id,name, inst_type, inst_name, )
+VALUES (1, 'Иван Иваныч', 'Пианино', 'Клавишный');
