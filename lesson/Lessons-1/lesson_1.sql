d3924bb78243170e34fff020b5e6795982f3e4f0
pr0h03iy <kireev.ruslan019@gmail.com>
2025-11-20 15:19:51 +0300

    initial commit

--- lesson/Lessons-1/lesson_1.sql
+++ lesson/Lessons-1/lesson_1.sql
@@ -1,13 +1,5 @@
-SHOW CREATE TABLE persons;
-
-CREATE TABLE Kireev_Table (
-  id int DEFAULT NULL,
-  name varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
-
-ALTER TABLE Kireev_Table ADD COLUMN user_name TEXT;
-ALTER TABLE Kireev_Table ADD COLUMN inst_type varchar(100);
-ALTER TABLE Kireev_Table ADD COLUMN inst_name varchar(100);
-INSERT INTO Kireev_Table (id,name, inst_type, inst_name, )
-VALUES ('game', 'nft');
+CREATE TABLE persons (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3
